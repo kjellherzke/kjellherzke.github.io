0573a7ab48759d1bfcc57e9dd0454eedf7819331
kjellherzke <kjell.herzke@gmail.com>
2024-05-14 20:38:13 +0200

    github wrapper finished; still not good...???

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import Grid, { GridListItem } from "./Grid";
-
-const gridTechnologiesList: GridListItem[] = [
-  {
-    id: "typescript",
-    color: "#004c7f",
-  },
-  {
-    id: "node",
-    color: "#00ee00",
-  },
-];
+import { GridRepo, GridRepoAuto, GridTech } from "./Grid";
 
 function App() {
   return (
-    <div className="w-full h-full flex justify-center py-3">
+    <div className="w-full h-screen flex justify-center py-3">
       <div className="w-3/4">
         <div className="flex items-center justify-between mb-12">
           <img className="w-9" src="/profile-pic.svg" />
-          <button className="rounded-full">
+          <a
+            href="https://github.com/kjellherzke"
+            className="rounded-full button"
+          >
             <img className="w-5" src="/icons/github.svg" />
-          </button>
+          </a>
         </div>
         <h1 className="text-3xl font-bold">Kjell,</h1>
         <h2 className="text-3xl">full-stack developer</h2>
@@ -27,10 +19,26 @@ function App() {
           As a programmer I enjoy building and architecturing software, using
           these <a href="#technologies">technologies</a>.
         </h3>
-        <h4 className="border-b text-xl pb-1 mb-3 border-b-slate text-secondary">
-          Technologies
+        {/*<h4 className="border-b text-xl pb-1 mb-3 border-b-slate text-secondary">
+          Projects
+        </h4> */}
+        {/*
+        <GridTech
+          data={[
+            {
+              id: "typescript",
+              color: "#004c7f",
+            },
+            {
+              id: "node",
+              color: "#00ee00",
+            },
+          ]}
+        />*/}
+        <h4 className="border-b text-xl pb-2 mb-6 border-b-slate text-secondary">
+          Projects
         </h4>
-        <Grid list={gridTechnologiesList} />
+        <GridRepoAuto />
       </div>
     </div>
   );
