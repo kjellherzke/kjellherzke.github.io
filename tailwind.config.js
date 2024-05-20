7b2d46c914636115ef685d1e4941b71111feb1c3
kjellherzke <kjell.herzke@gmail.com>
2024-05-20 12:13:46 +0200

    v3; better design; more appleaing...

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: "Space Grotesk",
+    },
     extend: {
       colors: {
-        primary: "#e7e7e7",
-        secondary: "#262626",
-        slate: "#131313",
-        bg: "#0e0e0e",
+        primary: "#FFFFFF",
+        secondary: "#8A8A8A",
+        background: "#0E0E0E",
       },
     },
   },
