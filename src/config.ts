6e77b51d77861afb56c2bfd2bc82d3ad186f90f0
kjellherzke <kjell.herzke@gmail.com>
2024-06-02 15:59:52 +0200

    feat: showcasing beacon

--- src/config.ts
+++ src/config.ts
@@ -12,19 +12,25 @@ interface MetaInfo {
 
 export const metaProjects: MetaInfo[] = [
   {
+    title: "Nightfall Dimmed Theme",
+    description: "Dark Visual Studio Code theme with +1000 downloads",
+    linkUrl: "https://github.com/kjellherzke/nightfall-theme.git",
+    // logoUrl:
+    // "https://raw.githubusercontent.com/kjellherzke/nightfall-theme/main/images/icon.png",
+  },
+  {
+    title: "Beacon - Online Visual Library",
+    description:
+      "(Work in progress) A visual tree-graph website showcasing in nodes the relativity of topics of all kind.",
+    linkUrl: "https://kjellherzke.github.io/beacon/",
+  },
+  {
     title: "Online Streaming Platform",
     description:
       "Develop a Progressive Web Application (PWA) streaming website utilizing OAuth for authentication and MongoDB for database management. Leverage Next.js for server-side rendering, Zod for schema validation, and TailwindCSS for styling. Ensure the application is written in TypeScript, prioritizing both performance and user accessibility.",
     from: "2023",
     to: "2023",
     // logoUrl: "/projects/griiice-logo.png",
-  },
-  {
-    title: "Nightfall Dimmed Theme",
-    description: "Dark Visual Studio Code theme with +1000 downloads",
-    linkUrl: "https://github.com/kjellherzke/nightfall-theme.git",
-    // logoUrl:
-    // "https://raw.githubusercontent.com/kjellherzke/nightfall-theme/main/images/icon.png",
   },
   {
     title: "Social Network Platform",
